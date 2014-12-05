a69d48aa8588544c336fc4ab984564dcd237a99d
ramonwyss <ramon.wyss@stud.hslu.ch>
2014-12-05 08:59:23 +0100

    GameView updated

--- src/fourG/view/GameView.java
+++ src/fourG/view/GameView.java
@@ -1,6 +1,5 @@
 package fourG.view;
 
-
 import fourG.controlling.MgmtController;
 import fourG.controlling.GameController;
 import fourG.model.IGameModelInformations;
@@ -15,19 +14,18 @@ import javax.swing.*;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author vogi23
  */
 public class GameView extends JFrame implements IModelObserver {
-    
+
     private MgmtController mgmt;
     private GameController game;
     private IGameModelInformations model;
-    
+
     private int numberOfRows = 8;
-    
+
     JFrame gui;
     JPanel informationPanel = new JPanel();
     JLabel yourName = new JLabel();
@@ -36,7 +34,7 @@ public class GameView extends JFrame implements IModelObserver {
     JLabel opponentIP = new JLabel();
     JPanel gamePanel = new JPanel();
     JLabel fourGInterface = new JLabel(); // für Darstellung des Interfaces, Mouse-Event
-    
+
     JMenuBar menuBar = new JMenuBar();
     JMenu menuFile = new JMenu("File");
     JMenuItem menuFileNewLocal = new JMenuItem("New Local Game");
@@ -47,23 +45,25 @@ public class GameView extends JFrame implements IModelObserver {
     JMenuItem menuFileExit = new JMenuItem("Exit");
     JMenu menuOptions = new JMenu("Options");
     JMenuItem setOptions = new JMenuItem("Set Options");
-            
-    public GameView(MgmtController mgmt, GameController game, IGameModelInformations model){
+    JFileChooser loadedGameChooser = new JFileChooser();
+    JFileChooser saveGameChooser = new JFileChooser();
+
+    public GameView(MgmtController mgmt, GameController game, IGameModelInformations model) {
         super("fourG");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
+
         this.mgmt = mgmt;
         this.game = game;
         this.model = model;
-        
+
         createMenu();
         createInterface();
         pack();
         setVisible(true);
-        
+
     }
-    
-    private void createMenu(){
+
+    private void createMenu() {
         menuFile.add(menuFileNewLocal);
         menuFileNewLocal.addActionListener(
                 new ActionListener() {
@@ -105,7 +105,8 @@ public class GameView extends JFrame implements IModelObserver {
                 new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                       // mgmt.initLoadedGame(File f);
+                        String f = getFileName();
+                        //mgmt.initLoadedGame(File f);
                     }
                 });
         menuFile.addSeparator();
@@ -123,7 +124,7 @@ public class GameView extends JFrame implements IModelObserver {
         setJMenuBar(menuBar);
     }
 
-    private void createInterface(){
+    private void createInterface() {
         setLayout(new GridLayout(1, 2));
         add(informationPanel);
         informationPanel.setLayout(new BoxLayout(informationPanel, BoxLayout.PAGE_AXIS));
@@ -134,25 +135,53 @@ public class GameView extends JFrame implements IModelObserver {
         add(gamePanel);
         gamePanel.add(fourGInterface);
         fourGInterface.addMouseListener(
-                new MouseListener(){
-                     @Override
-                     public void mouseClicked(MouseEvent e){
-                         fourGAction();
-                     }
+                new MouseListener() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        fourGAction();
+                    }
+
+                    @Override
+                    public void mousePressed(MouseEvent e) {
+                        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public void mouseReleased(MouseEvent e) {
+                        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public void mouseEntered(MouseEvent e) {
+                        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+
+                    @Override
+                    public void mouseExited(MouseEvent e) {
+                        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
                 });
     }
-    
+
     private void onFileExit() {
         System.exit(10);
     }
-    
-    private int fourGAction(){
+
+    private int fourGAction() {
         return getX();
     }
-    
+
+    private String getFileName() {
+        int r = loadedGameChooser.showOpenDialog(this);
+        String s = "no File!";
+        if (r == JFileChooser.APPROVE_OPTION) {
+            s = loadedGameChooser.getSelectedFile().getName();
+        }
+        return s;
+    }
+
     @Override
     public void update() {
-           System.err.println("GameView.update noch nicht implementiert");
+        //  System.err.println("GameView.update noch nicht implementiert");
     }
-    
 }
